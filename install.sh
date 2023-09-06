b88dd9d045ce3ab42555eed91049ff12b9fc0552
Samuel Loewen <samuellwn@gmail.com>
2023-09-06 12:59:28 -0400

    Make install.sh work on Mac OS X

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-install -Dm644 nvim/init.lua ~/.config/nvim/init.lua
-install -Dm644 nvim/ginit.vim ~/.config/nvim/ginit.vim
-install -Dm644 nvim/dracowizard.lua ~/.config/nvim/colors/dracowizard.lua
-install -Dm644 nvim/nvim-qt.conf ~/.config/nvim-qt/nvim-qt.conf
+install -d -m 755 ~/.config
+install -d -m 755 ~/.config/nvim
+install -d -m 755 ~/.config/nvim-qt
+
+install -m 644 nvim/init.lua ~/.config/nvim/init.lua
+install -m 644 nvim/ginit.vim ~/.config/nvim/ginit.vim
+install -m 644 nvim/dracowizard.lua ~/.config/nvim/colors/dracowizard.lua
+install -m 644 nvim/nvim-qt.conf ~/.config/nvim-qt/nvim-qt.conf
