2880c860461962acaf434344de0d388fb1be2eb0
Samuel Loewen <samuellwn@gmail.com>
2023-10-11 16:11:20 -0400

    diffopt filler hurts more than it helps

--- nvim/init.lua
+++ nvim/init.lua
@@ -41,6 +41,8 @@ g.ale_sign_column_always = 1
 g.ale_linters_ignore = { 'mcs', 'mcsc', 'csc' }
 g.OmniSharp_server_use_net6 = 1
 
+cmd('set diffopt-=filler')
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
