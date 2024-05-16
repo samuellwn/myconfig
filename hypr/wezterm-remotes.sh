dde351f09f490255d3d9dad43347eadaad686382
Samuel Loewen <samuellwn@gmail.com>
2024-05-16 11:49:43 -0400

    Some wezterm remote fixes

--- hypr/wezterm-remotes.sh
+++ hypr/wezterm-remotes.sh
@@ -8,11 +8,13 @@ declare -A wezterms forwards
 
 # these arrays link gui name choices to commands
 wezterms[hoard]="wezterm connect hoard"
-forwards[hoard]="ssh hoard -N -R /Users/dracowizard/.gnupg/S.gpg-agent:/run/user/1000/gnupg/S.gpg-agent.extra -R /Users/dracowizard/.gnupg/S.gpg-agent.ssh:/run/user/1000/gnupg/S.gpg-agent.ssh"
+forwards[hoard]="ssh hoard -nN -R /Users/dracowizard/.gnupg/S.gpg-agent:/run/user/1000/gnupg/S.gpg-agent.extra -R /Users/dracowizard/.gnupg/S.gpg-agent.ssh:/run/user/1000/gnupg/S.gpg-agent.ssh -R 10999:127.0.0.1:10999"
 wezterms[joshair]="wezterm connect joshair"
-forwards[joshair]="ssh joshair -N -R /Users/dracowizard/.gnupg/S.gpg-agent:/run/user/1000/gnupg/S.gpg-agent.extra -R /Users/dracowizard/.gnupg/S.gpg-agent.ssh:/run/user/1000/gnupg/S.gpg-agent.ssh"
+forwards[joshair]="ssh joshair -nN -R /Users/dracowizard/.gnupg/S.gpg-agent:/run/user/1000/gnupg/S.gpg-agent.extra -R /Users/dracowizard/.gnupg/S.gpg-agent.ssh:/run/user/1000/gnupg/S.gpg-agent.ssh"
 wezterms[imac]="wezterm connect imac"
-forwards[imac]="ssh imac -N -R /Users/dracowizard/.gnupg/S.gpg-agent:/run/user/1000/gnupg/S.gpg-agent.extra -R /Users/dracowizard/.gnupg/S.gpg-agent.ssh:/run/user/1000/gnupg/S.gpg-agent.ssh"
+forwards[imac]="ssh imac -nN -R /Users/dracowizard/.gnupg/S.gpg-agent:/run/user/1000/gnupg/S.gpg-agent.extra -R /Users/dracowizard/.gnupg/S.gpg-agent.ssh:/run/user/1000/gnupg/S.gpg-agent.ssh"
+wezterms[roosh]="wezterm connect roosh"
+wezterms[roosh]="ssh roosh -nN -R /run/user/999/gnupg/S.gpg-agent:/run/user/1000/S.gpg-agent.extra -R /run/user/999/gnupg/S.gpg-agent.ssh:/run/user/1000/S.gpg-agent.ssh"
 
 # prompt the user to select a host
 host=$(echo ${(Fk)wezterms} | wofi --show=dmenu)
@@ -29,6 +31,9 @@ if [[ -n ${forwards[$host]} ]]; then
 	if [[ -f $pidfile ]]; then
 		kill $(<$pidfile) && rm $pidfile
 	fi
+
+	# make sure the local gpg-agent is running
+	gpg-connect-agent /bye
 
 	${=forwards[$host]} &
 	echo $! > $pidfile
