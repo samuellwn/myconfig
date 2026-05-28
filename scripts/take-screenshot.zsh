93c8ec7171c6243a9c33e3764253683d66d57cb1
Samuel Loewen <samuellwn@gmail.com>
2026-05-28 15:04:43 -0400

    Fix screenshot script

--- scripts/take-screenshot.zsh
+++ scripts/take-screenshot.zsh
@@ -9,20 +9,25 @@ filename="$HOME/Pictures/Screenshots/$(date +'%Y-%m-%d_%H-%M-%S').png"
 
 hyprpicker_pid=
 
+hyprctl keyword animations:enabled no &>/dev/null
+sleep 0.1
+
 if command -v hyprpicker &>/dev/null; then
 	hyprpicker -r -z &
-	sleep 0.2
+	sleep 0.1
 	hyprpicker_pid=$!
 	TRAPEXIT() {
-		kill $hyprpicker_pid
+		kill $hyprpicker_pid &>/dev/null || true
 	}
 fi
 
 if command -v gpu-screen-recorder &>/dev/null; then
-	gpu-screen-recorder -w region -region $(slurp -do -f "%wx%h+%x+%y") \
+	gpu-screen-recorder -w $(slurp -do -f "%wx%h+%x+%y") \
 		-cursor no -v no -o $filename
 else
 	grim -g $(slurp -do) $filename
 fi
+
+hyprctl keyword animations:enabled yes &>/dev/null || true
 
 wl-copy --type image/png <$filename
